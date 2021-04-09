e2b8769c69263109961e0d3541fe7cca5bd0c8c2
kce360 <360kce360@gmail.com>
2021-04-09 19:10:12 +0300

    new changes

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
-var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var restRouter = require('./routes/messages');
 
 var app = express();
-
-///////
-/*app.use(express.json({limit: '1mb'}));
-app.post('/:id', (req, res) => {
-  console.log("app is here");
-  console.log(request.body);
-})*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,15 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: "kissanviikset"
-}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/messages', restRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -52,9 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
-
-
 
 module.exports = app;
