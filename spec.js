83497177b8e710910d146076bd4652bb53ddb796
Animotor <hannu@animoottori.net>
2021-04-15 08:12:15 +0300

    JEST: working log i/o test

--- spec.js
+++ spec.js
@@ -1,15 +1,36 @@
 const request = require('supertest');
 const app = require('./app');
 
+// Luodaan käyttäjätunnus
+test('Luoda uusi käyttäjä', async ()=> {
+    await request(app).post('/kayttajat/rekisteroityminen').send({
+        userid:'brlebbo',
+        passwd:'bebbo',
+        passwd:'bebbo'})
+        .expect('Found. Redirecting to /');
+    
+});
+
+// Kirjaudutaan palveluun
 test('Pitäisi kirjautua palveluun nimellä Gösta', async ()=> {
     const body = await request(app).post('/kayttajat/kirjautuminen').send({
         userid:'Gösta',
         passwd:'salasana'})
         .expect('Found. Redirecting to /');
-    console.log(body.res.req);
+    //console.log(body.res.req);
 });
 
-test('Löytyykö /kirjautuminen -sivu', async ()=> {
-    const req = request(app).get('/kayttajat/kirjautuminen');
+// Aloitetaan uusi keskustelu
+
+// Kirjoitetaan keskusteluun toinen viesti
+
+// Muokataan viestiä
+
+// Poistetaan viesti
+
+// Kirjaudutaan ulos
+test('Pitäisi kirjautua ulos (fake)', async ()=> {
+    // tämä testaa oikeasti, ohjautuuko käyttäjä kirjautumissivun sijaan ulos, kuten tapahtuu, kun kirjautunut käyttäjä yrittää mennä kirjautumissivulle. 
+    const req = request(app).get('/kayttajat/kirjautuminen').expect(451);
     //console.log(req.session);
 })
