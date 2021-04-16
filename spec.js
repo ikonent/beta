d615e7e0cb99763baded85e176d9eaf4a6d54c31
Animotor <hannu@animoottori.net>
2021-04-16 21:25:14 +0300

    Jest: removed async -code from register, log i/o tests

--- spec.js
+++ spec.js
@@ -1,27 +1,68 @@
 const request = require('supertest');
 const app = require('./app');
+
 /*
-// Luodaan käyttäjätunnus
-test('Luoda uusi käyttäjä', async ()=> {
-    await request(app).post('/kayttajat/rekisteroityminen').send({
+// Callback, joka odottaa register-testin toteutumista.
+function testRegister(callback) {
+    // set time for how long to wait for the command
+  setTimeout(() => {
+      // this is the actual test
+    const tulos = request(app).post('/kayttajat/rekisteroityminen').send({
         userid:'brlebbo',
         passwd:'bebbo',
-        passwd:'bebbo'})
-        .expect('Found. Redirecting to /');
-    
+        passwd:'bebbo'});
+    // callback palauttaa tuloksen sinne, mistä funktiota kutsuttiin
+    callback(tulos);
+  }, 500);
+}
+
+
+// Luodaan käyttäjätunnus
+test('Pitäisi luoda uusi käyttäjä', done => {
+    testRegister(tulos =>{ 
+        expect(tulos).toBe('Found. Redirecting to /');
+    });
+    done();            
 });
 */
+/*
+// Callback, joka odottaa logIn-testin toteutumista.
+function testLogIn(callback) {
+    // set time for how long to wait for the command
+  setTimeout(() => {
+      // this is the actual test
+    const tulos = request(app).post('/kayttajat/kirjautuminen').send({
+            userid:'brlebbo',
+            passwd:'bebbo'});
+    // callback palauttaa tuloksen sinne, mistä funktiota kutsuttiin
+    callback(tulos);
+  }, 500)
+}
+
 // Kirjaudutaan palveluun
 test('Pitäisi kirjautua palveluun nimellä brlebbo', done => {
-    addAsync(10,5, result =>{
-        const body = request(app).post('/kayttajat/kirjautuminen').send({
-            userid:'brlebbo',
-            passwd:'bebbo'})
-        .expect(result).toBe('Found. Redirecting to /');
-    //console.log(body.res.req);
+    // Kutsutaan ajallisesti rajattua funktiota, joka testaa.
+    // Lähetetään funktioon asynkroninen sisältö, 
+    // joka odottaa vastasta testiin muuttujaan 'tulos'.
+    // tämän 'tuloksen' odotetaan olevan: "Found. Redirecting to /"
+    testLogIn(tulos => {
+        expect(tulos).toBe('Found. Redirecting to /');
+        // Kun on saatu tulos, voidaan päättää testi
         done();
-     })
+    });
 });
+*/
+
+// Kirjaudutaan palveluun
+test('Pitäisi kirjautua palveluun nimellä brlebbo', done => {
+    const tulos = request(app).post('/kayttajat/kirjautuminen').send({
+            userid:'brlebbo',
+            passwd:'bebbo'}).expect('Found. Redirecting to /');
+        // Kun on saatu tulos, voidaan päättää testi
+        done();
+    });
+
+
 /*
 // Aloitetaan uusi keskustelu
 test('Pitäisi luoda uusi viesti', done => {
@@ -46,12 +87,31 @@ test('Pitäisi luoda uusi viesti', done => {
 // Poistetaan viesti
 
 // Kirjaudutaan ulos
+
+
+/*
+// Callback, joka odottaa register-testin toteutumista.
+function testLogOut(callback) {
+    // set time for how long to wait for the command
+  setTimeout(() => {
+      // this is the actual test
+    const tulos = request(app).get('/kayttajat/kirjautuminen')
+    // callback palauttaa tuloksen sinne, mistä funktiota kutsuttiin
+    callback(tulos);
+  }, 500);
+}
+
 test('Pitäisi kirjautua ulos (fake)', done => {
-    addAsync(10,5, result => {
-        // tämä testaa oikeasti, ohjautuuko käyttäjä kirjautumissivun sijaan ulos, kuten tapahtuu, kun kirjautunut käyttäjä yrittää mennä kirjautumissivulle. 
-        const req = request(app).get('/kayttajat/kirjautuminen').expect(result).toBe(451);
-        //console.log(req.session);
-        done();    
-    })
-    
-})
+    testLogOut(tulos => {
+        expect(tulos).toBe(451);
+        done();
+    });
+});
+*/
+
+
+test('Pitäisi kirjautua ulos (fake)', done => {
+    const tulos = request(app).get('/kayttajat/kirjautuminen').expect(451);
+        done();
+});
+
