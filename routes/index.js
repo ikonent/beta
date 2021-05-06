09a830a65533a716b173697af161d4709c23a7f8
Hannu Autti <2007373@edu.karelia.fi>
2021-05-06 20:40:12 +0300

    Added explanatory comments on index.js

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var session = require('express-session');
-var db = require('../dbOperations');
-const Localization = require("localizationjs");
-const locale = new Localization({ defaultLocale: "fi" });
+var db = require('../dbOperations');                        // Needed for database connection
+const Localization = require("localizationjs");             // Needed for localization
+const locale = new Localization({ defaultLocale: "fi" });   // Localization object to store dictionaries and information about locale
 
 //Dictionaries
-const dictionaryFI = require('../languages/fi');
-const dictionaryEN = require('../languages/en');
+const dictionaryFI = require('../languages/fi');            // Dictionary for Finnish
+const dictionaryEN = require('../languages/en');            // Dictionary for English
 // create a locale manager
 
 // Globaalit muuttujat
 
 // add the dictionaries
-locale.addDict("fi", dictionaryFI);
-locale.addDict("en", dictionaryEN);
+locale.addDict("fi", dictionaryFI);                         //  Add Finnish dictionary to locale
+locale.addDict("en", dictionaryEN);                         //  Add English dictionary to locale
 
 
 /* GET home page. */
 router.get('/:lang([a-z]{2})?/', function(req, res, next) {
-    //console.log(req);
-    //console.log(res);
-    //console.log(req.query);
-    //console.log(req.session);
+    /*
+        Display index page
+        // Check if language is set in URI
+    */
     // Check if there is an effort to change lang
-    
-    console.log("sesloc "+req.session.locale+" "+req.params.lang);
-    var add2Url ='';
-    if(req.session.locale === undefined) {
-        if (req.params.lang !== undefined && locale.hasDict(req.params.lang)) {
-            req.session.locale = req.params.lang;
-            locale.setCurrentLocale(req.session.locale);
+    var add2Url ='';                                        // Variable for locale-information to be used for href
+    if(req.session.locale === undefined) {                  // Check if value locale-string (EN or FI) is stored in session
+        if (req.params.lang !== undefined && locale.hasDict(req.params.lang)) {     //  Check if requested URI has locale set
+            req.session.locale = req.params.lang;                                   //  Assign locale in URI to session 
+            locale.setCurrentLocale(req.session.locale);                            //  Set current locale
         } else if (locale.getCurrentLocale()!==undefined){
-            req.session.locale = locale.getCurrentLocale().language;
+            req.session.locale = locale.getCurrentLocale().language;                //  Pick current locale to session locale String
         } else {
-            req.session.locale = locale.getDefaultLocale().language;
-            locale.setCurrentLocale(locale.getDefaultLocale().language);
+            req.session.locale = locale.getDefaultLocale().language;                //  Set session locale-String to match default locale
+            locale.setCurrentLocale(locale.getDefaultLocale().language);            //  Set current locale to match default locale
         }
     } else if(req.params.lang !== undefined && locale.hasDict(req.params.lang) && req.session.locale != req.params.lang ) {
-        req.session.locale = req.params.lang;
-        locale.setCurrentLocale(req.session.locale);
+        req.session.locale = req.params.lang;                                       //  Set session locale-string to match language setting in URI
+        locale.setCurrentLocale(req.session.locale);                                //  Set current locale to match session locale-String
         
     }
     // Change url-variable to have lang-parameter in urls
@@ -49,47 +47,51 @@ router.get('/:lang([a-z]{2})?/', function(req, res, next) {
     
 
     
-    
+    // Check if session has user ID stored
     if(req.session.userid === undefined || req.session.userid == "") {
+        // Set session user ID to null = user has not logged in
         req.session.userid = null;
     }
-
+    
+    // Check if there is a parameter for 'Newest first / Oldest first' setting in URI
     if(req.query.h_uusin!= undefined){
-        //console.log(req.query.h_uusin);
-        req.session.h_uusin = (req.query.h_uusin=='true');
-
+        // If set, store in session
+        req.session.h_uusin = (req.query.h_uusin=='true'); // value comes as string and needs to be converted into boolean
     } else if (req.session.h_uusin === undefined) {
+        // If not set, set value to true
         req.session.h_uusin = true;
     }
-
+    
+     // Check if there is a parameter for 'truncate / expand messages' setting in URI
     if(req.query.h_tiivis != undefined){
-        req.session.h_tiivis = (req.query.h_tiivis == 'true');
-
+        req.session.h_tiivis = (req.query.h_tiivis == 'true'); // value comes as string and needs to be converted into boolean
     } else if (req.session.h_tiivis === undefined) {
+        // If not set, set value to false
         req.session.h_tiivis = false;
     }
     //console.log("jotain tapahtu "+req.session.h_uusin);
     db.findTitles(req.session.h_uusin,function(msg) {
-        return res.status(200).render('index', {
-            dict:locale,
-            addToUrl:add2Url,
-            title: locale.translate('index.title'),
-            login: req.session.userid,
-            messages: msg,
-            topikki:'',
-            currUrl:'/',
-            urlSuffix:'?',
-            onLogPage:false,
-            onSignPage:false,
-            p_uusin:true,                       // Is newest message on top button visible
-            p_tiivis:false,                     // Is condense messages button visible
-            h_uusin:req.session.h_uusin,        // value of newest on top button
-            h_tiivis:req.session.h_tiivis,      // value of condense messages button
+        return res.status(200).render('index', {    
+            dict:locale,                            // Locale and dictionaries
+            addToUrl:add2Url,                       // locale-name information for URI. Format: 'fi/'
+            title: locale.translate('index.title'), // Title of page
+            login: req.session.userid,              // Information about logged user - this is probably stored on a cookie but I couldn't access it
+            messages: msg,                          // Array of messages - or message topics in this case
+            topikki:'',                             // Name of the topic that is currently viewed. Actually valid only on 'keskustelu_aiheesta'
+            currUrl:'/',                            // reference to current page
+            urlSuffix:'?',                          // Value to add last on buttons with href to (current) page
+            onLogPage:false,                        // Variable to tell UI buttons in hallinta.ejs that this is not log in-page
+            onSignPage:false,                       // Variable to tell UI buttons in hallinta.ejs that this is not add user-page
+            p_uusin:true,                           // Is newest message on top button visible
+            p_tiivis:false,                         // Is condense messages button visible
+            h_uusin:req.session.h_uusin,            // value of newest on top button
+            h_tiivis:req.session.h_tiivis,          // value of condense messages button
             p_tietoa:true   
         }); 
     });
 });
-
+// Variable has uri names for about pages
+// It would be better if these values were derived from the dictionaries
 const tietoa_urls = [
     'tietoa',
     'about'
@@ -99,22 +101,18 @@ const tietoa_urls = [
 router.get('/:page('+tietoa_urls.join('|')+')/:lang([a-z]{2})?/', function(req, res) {
     // Check if there is an effort to change lang
     var add2Url ='';
-    console.log("sesloc "+req.session.locale+" "+req.params.lang+" "+req.url)
-     if(req.session.locale === undefined) {
+    if(req.session.locale === undefined) {
         if (req.params.lang !== undefined && locale.hasDict(req.params.lang)) {
-            console.log("nollasta "+req.params.lang)
             req.session.locale = req.params.lang;
             locale.setCurrentLocale(req.session.locale);
         } else if (locale.getCurrentLocale()!==undefined){
             console.log("nykyinne "+locale.getCurrentLocale().language)
             req.session.locale = locale.getCurrentLocale().language;
         } else {
-            console.log("oletus "+locale.getDefaultLocale())
             req.session.locale = locale.getDefaultLocale().language;
             locale.setCurrentLocale(locale.getDefaultLocale().language);
         }
     } else if(req.params.lang !== undefined && locale.hasDict(req.params.lang) && req.session.locale != req.params.lang ) {
-        console.log("muutos "+req.params.lang)
         req.session.locale = req.params.lang;
         locale.setCurrentLocale(req.session.locale);
         
@@ -176,10 +174,7 @@ router.get('/:page('+keskustelu_aiheesta_urls.join('|')+')/:lang([a-z]{2})?/', f
     if(req.session.locale != locale.getDefaultLocale()){
         add2Url = req.session.locale+'/';  // JOS ei oletuskieli, NIIN annetaan täydennysURL:iin
     }
-    
-    
-    
-    
+
     if(req.session.userid === undefined || req.session.userid == "") {
         req.session.userid = null;
     }
@@ -200,15 +195,11 @@ router.get('/:page('+keskustelu_aiheesta_urls.join('|')+')/:lang([a-z]{2})?/', f
     }
 
     if(req.query.delid != undefined){
-        /*/console.log("poistetaan id: "+req.query.delid); */
         db.deleteMessage(req.query.delid, req.session.userid, function(rvalue) {
             if(rvalue && req.query.id != undefined){
-                console.log("Onnistui?! paluu keskusteluun.");
                 return res.status(302).redirect('./?id='+req.query.id);
 
             } else if (req.query.id === undefined){
-
-                //console.log("Onnistui?! paluu juureen.");
                 return res.status(302).redirect('/');
             } else {
                 return res.render('alert', {
@@ -228,27 +219,25 @@ router.get('/:page('+keskustelu_aiheesta_urls.join('|')+')/:lang([a-z]{2})?/', f
                     h_tiivis:req.session.h_tiivis,
                     p_tietoa:true
                 });
-                //console.log("Pieleen meni");
             }
         });
     } else if(req.query.id!= undefined){
         db.findMessages(req.query.id,req.session.h_uusin,function(msg) {
             return res.status(200).render('keskustelu_aiheesta', { title: 'Keskustelu',
-                  dict:locale,
-                  addToUrl:add2Url,
-                  login: req.session.userid,
-                  messages:msg,
-                  onLogPage:false,
-                  onSignPage:false,
-                  topikki:req.query.id,
+                dict:locale,
+                addToUrl:add2Url,
+                login: req.session.userid,
+                messages:msg,
+                onLogPage:false,
+                onSignPage:false,
+                topikki:req.query.id,
                 currUrl:'/'+locale.translate("urls.keskustelu")+'/',
-
                 urlSuffix:'?id='+req.query.id+'&',
-                  p_uusin:true,
-                  p_tiivis:true,
-                  h_uusin:req.session.h_uusin,
-                  h_tiivis:req.session.h_tiivis,
-                  p_tietoa:true });
+                p_uusin:true,
+                p_tiivis:true,
+                h_uusin:req.session.h_uusin,
+                h_tiivis:req.session.h_tiivis,
+                p_tietoa:true });
         });
     } else {
         return res.status(302).redirect('/');
@@ -454,8 +443,6 @@ router.get('/:page('+viesti_urls.join('|')+')/:lang([a-z]{2})?/', function(req, 
         });
     } else {
         // Muussa tapauksessa kirjoittaja aloittaa uuden viestiketjun
-
-
         return res.status(200).render('uusi_viesti', {
             title: locale.translate("uusi_viesti.title"),
             dict:locale,
